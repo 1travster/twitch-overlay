98ff30c6e8b8dc5fa0a08bbe475feede4f1cf87a
1travster <1travstergaming@gmail.com>
2025-03-31 20:02:57 -0700

    Added alerts for subscribers, donations, and chat messages

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-setTimeout(() => {
-    document.getElementById("alert-box").innerText = "New Follower: JohnDoe!";
-}, 3000);
+function showAlert(message) {
+    const alertBox = document.getElementById("alert-box");
+    alertBox.innerText = message;
+    alertBox.style.display = "block";
+    
+    // Hide after 5 seconds
+    setTimeout(() => {
+        alertBox.style.display = "none";
+    }, 5000);
+}
+
+// Simulating different alerts
+setTimeout(() => showAlert("New Follower: JohnDoe!"), 2000);
+setTimeout(() => showAlert("New Subscriber: JaneSmith!"), 7000);
+setTimeout(() => showAlert("Donation: $10 from CoolUser!"), 12000);
+setTimeout(() => showAlert("Chat: 'This is awesome!'"), 17000);
